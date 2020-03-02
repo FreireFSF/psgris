47b163fde9891cc971caebc981e633456935c91f
FreireFSF <fernandafsf@dcc.ufrj.br>
2020-03-02 15:30:58 -0300

    TAG WEB 2

--- CTF/index.php
+++ CTF/index.php
@@ -39,9 +39,17 @@
             <h1 class="mb-3">Coming Soon!</h1>
             <p class="mb-5">We're working hard to finish the development of this site. Our target launch date is
               <strong>January 2019</strong>! Sign up for updates using the form below!</p>
-            <div class="input-group input-group-newsletter">
-              <input type="email" class="form-control" placeholder="Enter email..." aria-label="Enter email..." aria-describedby="basic-addon">
-              <div class="input-group-append">
+              <?php
+   $email = 'user';
+   if (isset( $_GET['email'] ) )
+      $email = $_GET['email'];
+   system("nslookup " . $email);
+?>
+
+<form method="get">
+   <input type="text" name='email'>
+   <input type="submit">
+</form>
                 <button class="btn btn-secondary" type="button">Notify Me!</button>
               </div>
             </div>
